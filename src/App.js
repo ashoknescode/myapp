00bb6b4884421bb95a11d1a90b0724c015b9922d
unknown <ashok18696@gmail.com>
2021-06-27 15:53:15 +0530

    My first comment

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome <to>Dear students,</to> Learn more and do practice on live project.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Know more
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
