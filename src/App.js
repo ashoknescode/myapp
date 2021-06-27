70817abe85fcccec7fbefb3648721e806d53f99d
unknown <ashok18696@gmail.com>
2021-06-27 20:25:14 +0530

    Update app.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome <to>Dear students,</to> Learn more and do practice on live project.
+          Welcome <to>Dear students,</to> Learn more and do practice on live project. We will fight together.
         </p>
         <a
           className="App-link"
